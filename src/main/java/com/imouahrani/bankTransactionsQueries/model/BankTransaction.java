ee44a711f4902ea4834cc731f05d2b5142805d23
Imene <imene.ouahrani@capgemini.com>
2024-04-17 16:39:19 +0200

    KAFKA-05 : add bank transaction

--- src/main/java/com/imouahrani/bankTransactionsQueries/model/BankTransaction.java
+++ src/main/java/com/imouahrani/bankTransactionsQueries/model/BankTransaction.java
@@ -1,2 +1,29 @@
-package com.imouahrani.bankTransactionsQueries.model;public class BankTransaction {
+package com.imouahrani.bankTransactionsQueries.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BankTransaction {
+
+    private Long id;
+    private Long balanceId;
+    private BigDecimal amount;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "dd-MM-yyyy hh:mm:ss")
+    public Date time;
+    @Builder.Default
+    public BankTransactionState state = BankTransactionState.CREATED;
+
+
 }
+
