a1ec32d5bdbfdf880b45b5de806e0b7d518b60de
Imene <imene.ouahrani@capgemini.com>
2024-04-17 16:46:47 +0200

    KAFKA-06 : add bank balance

---
+++ src/main/java/com/imouahrani/bankTransactionsQueries/model/BankBalance.java
@@ -0,0 +1,36 @@
+package com.imouahrani.bankTransactionsQueries.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankBalance {
+
+    private Long id;
+    private BigDecimal amount = BigDecimal.ZERO;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "dd-MM-yyyy hh:mm:ss")
+    private Date lastUpdate;
+    private BankTransaction latestTransaction;
+
+    public BankBalance process(BankTransaction bankTransaction) {
+        this.id = bankTransaction.getBalanceId();
+        this.latestTransaction = bankTransaction;
+        if(this.amount.add(bankTransaction.getAmount()).compareTo(BigDecimal.ZERO) >= 0) {
+            this.latestTransaction.setState(BankTransactionState.APPROVED);
+            this.amount = this.amount.add(bankTransaction.getAmount());
+        } else {
+            this.latestTransaction.setState(BankTransactionState.REJECTED);
+        }
+        this.lastUpdate = bankTransaction.getTime();
+        return this;
+    }
+}
+
